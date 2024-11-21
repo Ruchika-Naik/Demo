223afa5780540234c5fb52ff9755cb00af08271f
Debadtta <Debadtta@LAPTOP-9GNQ199S>
2024-11-21 19:12:41 +0530

    DemoABC 1st commit and export

---
+++ DemoABC/src/demo/Demo.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hi i m Ruchika");
+		System.out.println("This is a Demo World");
+		System.out.println("Welcome");
+		
+		
+	}
+}
